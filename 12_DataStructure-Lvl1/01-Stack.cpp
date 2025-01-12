1568ef7556a325743df3ffac6bc11defad196389
AhmadHirzallah <ahmad.hirzallah.pro@gmail.com>
2025-01-12 16:09:36 +0300

    finishing-datastructure-day

---
+++ 12_DataStructure-Lvl1/01-Stack.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stack>        //  Stack is part of STL : Standard Template Library
+
+int main()
+{
+    system("clear");
+
+    std::stack <int> stkNumbers;
+
+    stkNumbers.push(10);
+    stkNumbers.push(20);
+    stkNumbers.push(30);
+    stkNumbers.push(40);
+    stkNumbers.push(50);
+
+    std::cout << "\nSize of (Count) the Stack of Numbers is:  " << stkNumbers.size() << std::endl << std::endl;
+
+    while (!(stkNumbers.empty()))
+    {
+        std::cout << stkNumbers.top() << "\n" << std::endl;
+        stkNumbers.pop();
+    }
+
+
+
+    std::cout << "\n\n------------\n\n Swapping ! :" << std::endl;
+
+    std::stack  <float> S1;
+    std::stack  <float> S2;
+
+    S1.push(10.1);
+    S1.push(20.2);
+    S1.push(30.3);
+    S1.push(40.4);
+    S1.push(50.5);
+
+
+    S2.push(60.6);
+    S2.push(70.7);
+    S2.push(80.8);
+    S2.push(90.9);
+    S2.push(100.110);
+    S2.push(100.110);
+
+
+    S1.swap(S2);
+
+
+    std::cout << std::endl << "Stack (1) After:   " ;
+    while (!(S1.empty()))
+    {
+        std::cout << S1.top() << "   ";
+        S1.pop();
+    }
+
+    std::cout << std::endl << "Stack (2) After:   " ;
+    while (!(S2.empty()))
+    {
+        std::cout << S2.top() << "   ";
+        S2.pop();
+    }
+    std::cout << "\n\n------------\n\n";
+
+    return 0;
+}
---
+++ 12_DataStructure-Lvl1/02-Queue.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <queue>        //  Stack is part of STL : Standard Template Library
+
+int main()
+{
+    system("clear");
+
+    std::queue <float>   myQueue;
+
+    myQueue.push(10.1);
+    myQueue.push(20.2);
+    myQueue.push(30.3);
+    myQueue.push(40.4);
+
+    std::cout << "Size of Queue:  " <<  myQueue.size() << std::endl;
+    std::cout << "Front of Queue:  " <<  myQueue.front() << std::endl;
+    std::cout << "Back of Queue:  " <<  myQueue.back() << std::endl << std::endl;
+
+    std::cout << "Queue elements:  " << std::endl;
+    
+    while(!(myQueue.empty()))
+    {
+        std::cout << myQueue.front() << "  " ; 
+        myQueue.pop();
+    }
+
+    std::cout << "\n\n";
+
+
+
+    std::cout << "\n\n------------\n\n Swapping ! :" << std::endl;
+
+
+    std::queue <int>    firstQueue;
+    std::queue <int>    lastQueue;
+
+    firstQueue.push(10);
+    firstQueue.push(20);
+    firstQueue.push(30);
+    firstQueue.push(40);
+    firstQueue.push(90);
+
+    lastQueue.push(60);
+    lastQueue.push(70);
+    lastQueue.push(80);
+    lastQueue.push(90);
+
+    firstQueue.swap(lastQueue);
+
+    std::cout << std::endl << "First Queue:  ";
+    while(!(firstQueue.empty()))
+    {
+        std::cout << firstQueue.front() << "   "; 
+        firstQueue.pop();
+    }
+
+    std::cout << std::endl << "Last Queue:  ";
+    while(!(lastQueue.empty()))
+    {
+        std::cout << lastQueue.front() << "   "; 
+        lastQueue.pop();
+    }
+
+    return 0;
+}
